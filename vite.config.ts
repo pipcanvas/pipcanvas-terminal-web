7fb4d8ac89c9a44901d9fa2bd36d74fb2b06f1e3
jessicanguyen629 <jessicanguyen629@biznizmail.com>
2025-05-19 06:27:01 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer';
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '~': fileURLToPath(new URL('./', import.meta.url))
     }
-  }
+  },
+    css: {
+        postcss: {
+            plugins: [autoprefixer],
+        },
+    }
 })
