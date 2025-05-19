0eeb6d9dd22aa84e9bd7d7f780fe74c9b1b70e9e
jessicanguyen629 <jessicanguyen629@biznizmail.com>
2025-05-19 08:37:51 -0700

    Fix Tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,14 +50,6 @@ export default {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        success: {
-          DEFAULT: "hsl(var(--success))",
-          foreground: "hsl(var(--success-foreground))",
-        },
-        warning: {
-          DEFAULT: "hsl(var(--warning))",
-          foreground: "hsl(var(--warning-foreground))",
-        },
         gain: {
           DEFAULT: "hsl(var(--gain))",
           light: "hsl(var(--gain-light))",
